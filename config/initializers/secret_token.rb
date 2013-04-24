379c7c45764fa0d354d5044e89cb7d8f6d1adb5b
Leif Bladt <leif.bladt@gmx.de>
2013-04-24 21:15:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Busfest::Application.config.secret_key_base = '5789a08e8f046f74eef0397e0f78387c93214e91f8d7eb93980f09ea74e6e3c20e7ad018530cd2f931e6125b41a05e1b4cfbd7be3c3027fd0ec1cf6cd3ea8649'
