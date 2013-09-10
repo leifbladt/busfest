2227fb2f68d7eb83d3fcc75d50f90a10fbad15a9
Leif Bladt <leif.bladt@gmx.de>
2013-09-10 21:28:15 +0200

    Upgrade gems

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
 # Make sure the secret is at least 30 characters and all random,
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Busfest::Application.config.secret_key_base = '5789a08e8f046f74eef0397e0f78387c93214e91f8d7eb93980f09ea74e6e3c20e7ad018530cd2f931e6125b41a05e1b4cfbd7be3c3027fd0ec1cf6cd3ea8649'
+Busfest::Application.config.secret_key_base = '134ba7038873a99f37480d41a60376eee82a4360b1dac47ec4a9dd6195125acb0887a96ec03fe6ff1686f75f3e48cec1719c6d3f429bb9b39bc44482bb0902fd'
