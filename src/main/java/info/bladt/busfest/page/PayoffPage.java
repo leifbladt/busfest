5fd85ce237b6e734c223bba45572bdd15124d758
lbladt <leif.bladt@1und1.de>
2014-01-08 18:58:43 +0100

    Add (empty) payoff page

---
+++ src/main/java/info/bladt/busfest/page/PayoffPage.java
@@ -0,0 +1,11 @@
+package info.bladt.busfest.page;
+
+/**
+ * @author <a href="mailto:leif.bladt@1und1.de">Leif Bladt</a>
+ */
+public class PayoffPage extends BasePage {
+    @Override
+    protected String pageTitle() {
+        return "Abrechnung";
+    }
+}
