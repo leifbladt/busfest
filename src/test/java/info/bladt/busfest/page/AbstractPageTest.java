c9ee99b7b453495e4240cff80b03f5af4ff30669
Leif Bladt <leif.bladt@gmx.de>
2014-02-09 10:59:48 +0100

    Fix Spring configuration for tests

--- src/test/java/info/bladt/busfest/page/AbstractPageTest.java
+++ src/test/java/info/bladt/busfest/page/AbstractPageTest.java
@@ -3,6 +3,10 @@ package info.bladt.busfest.page;
 import info.bladt.busfest.BusfestApplication;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+import javax.servlet.ServletContext;
 
 /**
  * @author <a href="mailto:leif.bladt@1und1.de">Leif Bladt</a>
@@ -12,6 +16,18 @@ public class AbstractPageTest {
 
     @Before
     public void setUp() {
-        tester = new WicketTester(new BusfestApplication());
+        tester = new WicketTester(new BusfestApplication() {
+            @Override
+            public ServletContext getServletContext() {
+                ServletContext servletContext = super.getServletContext();
+                XmlWebApplicationContext applicationContext = new XmlWebApplicationContext();
+                applicationContext.setConfigLocation("classpath:applicationContext.xml");
+                applicationContext.setServletContext(servletContext);
+                applicationContext.refresh();
+                servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, applicationContext);
+
+                return servletContext;
+            }
+        });
     }
 }
